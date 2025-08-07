9d51b6cef5094c847fbee13829314be3c0797fc4
Bernhard-Jassir Hammelmann <bj.hammelmann@gmail.com>
2025-08-07 17:40:57 +0000

    Kerin Abbruchzeichen diesmal, aber auch keine Fehlerkorrektur (im GUI je

--- src/components/prayer-times-card.tsx
+++ src/components/prayer-times-card.tsx
@@ -171,7 +171,7 @@ export function PrayerTimesCard({
 
 
   return (
-    <div className="w-[20rem] mx-auto">
+    <div className="w-full max-w-[20rem] mx-auto">
       <Card className="w-full shadow-2xl shadow-primary/10 bg-card/40 border-primary/20">
         <CardHeader className="text-center pb-2 relative">
           <div className="absolute top-4 left-4 right-4 flex justify-between items-center">
